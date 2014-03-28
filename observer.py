4f76c67979804717a565548b05f345ee5b809710
Khanh Dao <khanh.dao@ngc.com>
2014-03-28 12:31:22 -0700

    add logging

--- observer.py
+++ observer.py
@@ -1,12 +1,30 @@
 __author__ = 'daokh'
 from abc import ABCMeta, abstractmethod
+import logging,logging.config
 
+import sys
+# Set log level based on whether a command line parameter exists.
+# _logformat = '%(asctime)s : %(levelname)s : %(name)s : %(message)s'
+# _handler = logging.FileHandler('myapp.log')
+#
+# if len(sys.argv) > 1 and sys.argv[1] == 'debug':
+#     logging.basicConfig(format=_logformat, level=logging.INFO, handler=_handler)
+# else:
+#     logging.basicConfig(format=_logformat, level=logging.WARNING, handler=_handler)
+#
+# logger = logging.getLogger(__name__)
+# logger.addHandler(_handler)
+
+
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger(__name__)
 
 class Sender(object):
     def __init__(self):
         self.observers = list()
 
     def register(self, observer):
+        logger.info('register')
         if observer not in self.observers:
             self.observers.append(observer)
             return True
@@ -14,6 +32,7 @@ class Sender(object):
             return False
 
     def unregister(self, observer):
+
         if observer in self.observers:
             return self.observers.remove(observer)
 
