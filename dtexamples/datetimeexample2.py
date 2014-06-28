af6e0c280bc030ad673f97eaa348850b0c169aaf
daokh <khanh.dao@gmail.com>
2014-06-28 16:51:33 -0700

    Update datetimeexample2.py

--- dtexamples/datetimeexample2.py
+++ dtexamples/datetimeexample2.py
@@ -8,11 +8,16 @@ import calendar
 
 #dt = datetime.datetime.strptime('24052010', "%d%m%Y").date()
 dt = datetime.datetime.strptime('01011970 00:00:00 UTC', "%d%m%Y %H:%M:%S %Z")
-#dt = dt.astimezone(pytz.UTC)
-# tz = pytz.timezone('America/Los_Angeles')
-# utc_dt = tz.localize(dt, is_dst=True).astimezone(pytz.utc)
 tuple = dt.timetuple()
 ans_time = time.mktime(tuple)
 ans_time = calendar.timegm(tuple)
+
+dt = datetime.datetime.strptime('Jan 01, 1970 12:00:00 AM UTC', "%b %d, %Y %I:%M:%S %p %Z")
+tuple = dt.timetuple()
+ans_time = time.mktime(tuple)
+ans_time = calendar.timegm(tuple)
+
+
+
 print
 
