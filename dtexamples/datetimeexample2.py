bcb0daec09275f3a6544f882b34675051d7788bf
khanh.dao <khanh.dao@gmail.com>
2014-07-21 21:18:29 -0700

    update time

--- dtexamples/datetimeexample2.py
+++ dtexamples/datetimeexample2.py
@@ -1,4 +1,21 @@
 __author__ = 'daokh'
+"""
+time.strptime
+string --> tuple (no timezone applied, so matches string)
+
+time.mktime
+local time tuple --> seconds since epoch (always local time)
+
+time.localtime
+seconds since epoch --> tuple in local timezone
+
+calendar.timegm
+tuple in UTC --> seconds since epoch
+
+time.gmtime
+seconds since epoch --> tuple in UTC
+
+"""
 import datetime
 import time
 import pytz
@@ -9,6 +26,8 @@ import calendar
 #dt = datetime.datetime.strptime('24052010', "%d%m%Y").date()
 dt = datetime.datetime.strptime('01011970 00:00:00 UTC', "%d%m%Y %H:%M:%S %Z")
 tuple = dt.timetuple()
+
+
 ans_time = time.mktime(tuple)
 ans_time = calendar.timegm(tuple)
 
@@ -17,7 +36,16 @@ tuple = dt.timetuple()
 ans_time = time.mktime(tuple)
 ans_time = calendar.timegm(tuple)
 
+localNow = datetime.datetime.now()
+utcNow = datetime.datetime.utcnow()
 
+localTuple = localNow.timetuple()
+utcTuple = utcNow.timetuple()
 
+localUnixTime = time.mktime(localTuple)
+utcUnixTime = calendar.timegm(utcTuple)
+
+localString = localNow.strftime('%Y-%m-%d %H:%M:%S %Z')
+utcString = utcNow.strftime('%Y-%m-%d %H:%M:%S %Z')
 print
 
